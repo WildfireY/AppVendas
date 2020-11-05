d66ed817f36a70d8bc5664ef55cf8f9b743b7884
wildfireY <luiz06lhap@gmail.com>
2020-11-05 14:25:38 -0300

    DepartamentosController usando Entity Framework

--- Vendas/Startup.cs
+++ Vendas/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Vendas.Data;
 
 namespace Vendas
 {
@@ -33,6 +35,9 @@ namespace Vendas
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<VendasContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("VendasContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
